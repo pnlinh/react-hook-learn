22fe08e4af39a1356df4c46475e57a2467281613
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-11-20 23:18:02 +0700

    use state p2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Playground from './Playground';
+import Paint from "./components/Paint";
 
 function App() {
     return (
         <div className="App">
             <Playground />
+            <Paint />
         </div>
     );
 }
